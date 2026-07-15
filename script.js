71d761e0f4b4f837e9664238507a513c7ef0edf8
Jose Ascuntar <joseaskuntar@gmail.com>
2026-07-15 12:15:47 -0500

    Add function for computer choice

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const GAMES_PER_ROUND = 5;
-
 function getComputerChoice(){
     let random = Math.floor(Math.random() * 3);
     switch(random) {
@@ -17,59 +15,4 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice() {
-    return prompt("Rock, Paper or Scissors?");
-}
-
-let humanScore = 0;
-let computerScore = 0;
-
-function playGame(humanChoice, computerChoice) {
-    let humanChoiceCap = humanChoice.at(0).toUpperCase();
-    for (let i = 1; i < humanChoice.length; i++) {
-        humanChoiceCap += humanChoice.at(i).toLowerCase();
-    }
-
-    let humanWon = false;
-    if ((humanChoiceCap == "Rock" && computerChoice == "Scissors") 
-     || (humanChoiceCap == "Scissors" && computerChoice == "Paper")
-     || (humanChoiceCap == "Paper" && computerChoice == "Rock")) {
-        humanWon = true;
-     } 
-
-    if (humanChoiceCap == computerChoice) {
-        return "You tied! You both got " + humanChoiceCap + ".";
-    } else if (humanWon) {
-        humanScore++;
-        return "You won! " + humanChoiceCap + " beats " + computerChoice + ".";
-    } else {
-        computerScore++;
-        return "You lose! " + computerChoice + " beats " + humanChoiceCap + ".";
-    }
-}
-
-function playRound() {
-    for (let i = 0; i < GAMES_PER_ROUND; i++) {
-        console.log(playGame(getHumanChoice(), getComputerChoice()));
-    }
-    if (humanScore > computerScore) {
-        let tempHuman = humanScore;
-        let tempComputer = computerScore;
-        humanScore = 0;
-        computerScore = 0;
-        return "YOU WON THE ROUND! you won " + tempHuman + " game" + (tempHuman == 1 ? "" : "s") + ", while the computer won " + tempComputer + ".";   
-    } else if (humanScore < computerScore) {
-        let tempHuman = humanScore;
-        let tempComputer = computerScore;
-        humanScore = 0;
-        computerScore = 0;
-        return "You LOST the round! you won " + tempHuman + " game" + (tempHuman == 1 ? "" : "s") + ", while the computer won " + tempComputer+ "."; 
-    } else {
-        let tempScore = humanScore;
-        humanScore = 0;
-        computerScore = 0;
-        return "You TIED the round! you each won " + tempScore + " game" + (tempScore == 1 ? "" : "s") + ".";
-    }
-}
-
-console.log(playRound());
+console.log(getComputerChoice())
